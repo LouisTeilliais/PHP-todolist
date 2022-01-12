fd116d193721d064c5a477d2d2cff51a7083cea1
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-01-12 16:55:12 +0100

    ajout des todolist bloc finit, en cours delete bloc

--- toDoList.php
+++ toDoList.php
@@ -1,9 +1,41 @@
-<link rel="stylesheet" href="/css/index.css" type="text/css">
+<?php require('db_connect.php'); ?>
+
+<link rel="stylesheet" href="/css/todolist.css" type="text/css">
 <?php $title = 'Notre site web'; ?>  <!-- Debut du template -->
 
 <?php ob_start(); ?>
 <h1>Page TO DO LIST</h1>
-<p>LETS GO !!!</p>
+
+
+<?php 
+$todolist = $conn->prepare("SELECT * FROM todolist"); 
+$todolist->execute();
+$todos = $todolist->fetchAll();
+?>
+
+<div class="main-body">
+    <div class="add-post">
+        <form action="add.php" method="POST" autocomplete="off">
+            <input type="text" name="input_text"/> 
+            <button type="submit" class="btn"> + Create new</button>
+        </form>
+    </div>
+    <?php foreach($todos as $todo) { ?>
+        <div class="todo-item">
+        <form action="delete.php" method="POST" autocomplete="off">
+            <input type="checkbox">
+            <h2><?php echo $todo['ToDoListName'] ?></h2>
+            <button type="submit" name="delete" class="btn">Delete</button>
+        </form>
+        </div>
+    <?php } ?>
+<div>
+
+
+
 <?php $content = ob_get_clean(); ?> <!-- Fin du template -->
 
-<?php require('template.php'); ?>
+<?php require('template.php');?>
+
+
+
